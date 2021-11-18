b4ec64a8c5a2a159703737fb73b980dc32f739b7
Kyle Joseph Timajo <timajokyle24@gmail.com>
2021-11-19 01:33:20 +0800

    Added basic files and directories

---
+++ lib/components/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFF28B67E);
+  static const Color primaryColorLight = Color(0xFF575D75);
+
+  static const String fontFamily = 'Montserrat';
+
+  // static const TextTheme textTheme = TextTheme(
+
+  // );
+}
---
+++ lib/screens/home/home.dart
@@ -0,0 +1,12 @@
+import 'package:coconut_maturity_detector/screens/home/home_body.dart';
+import 'package:flutter/material.dart';
+
+// ignore: use_key_in_widget_constructors
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HomeBody(),
+    );
+  }
+}
