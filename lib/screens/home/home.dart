9c90db99b50c0a7ba9970afac78b40508494c75c
Kyle Joseph Timajo <timajokyle24@gmail.com>
2021-11-19 14:09:13 +0800

    Added partial UI to home screen
    - Added home screen header
    - Home screen body contents to be followed

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -5,8 +5,10 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: HomeBody(),
+    return SafeArea(
+      child: Scaffold(
+        body: HomeBody(),
+      ),
     );
   }
 }
--- lib/screens/home/home_body.dart
+++ lib/screens/home/home_body.dart
@@ -1,9 +1,57 @@
+import 'package:coconut_maturity_detector/components/theme.dart';
 import 'package:flutter/material.dart';
 
-// ignore: use_key_in_widget_constructors
+// ignore: use_key_in_widget_constructors, must_be_immutable
 class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
+        children: [
+          Container(
+            color: AppTheme.primaryColor,
+          ),
+          Column(
+            children: [
+              homeHeader(context),
+              homeBody(context),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget homeHeader(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.2,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          const Text(
+            'Coconut Maturity Detector',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget homeBody(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(40),
+            topRight: Radius.circular(40),
+          ),
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
